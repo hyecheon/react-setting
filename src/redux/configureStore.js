210a8c03aed525345ba3a2a187091b544ef4ef60
ihyecheon <ihyecheon@gmail.com>
2018-01-28 01:51:41 +0900

    yarn add reset-css
    reset-css 추가~!

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
 import {routerReducer, routerMiddleware} from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
+import {i18nState} from 'redux-i18n';
 import sample from './modules/sample'//import 리듀서
 
 const env = process.env.NODE_ENV;
@@ -18,11 +19,12 @@ if (env === "development") {
 
 const reducer = combineReducers({
 	routing: routerReducer,
+	i18nState,
 	sample
 });
 
 let store = initialState => createStore(reducer, applyMiddleware(...middleware));
 
-export { history };
+export {history};
 
 export default store();
---
+++ src/translations.js
@@ -0,0 +1,5 @@
+export const translations = {
+	ko: {
+		"Login In": "로그인"
+	}
+};
