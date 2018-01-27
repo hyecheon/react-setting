29dc164bc551432a38016acde5ad87d825f3ff24
ihyecheon <ihyecheon@gmail.com>
2018-01-28 00:38:27 +0900

    yarn add redux-thunk 추가
    middleware 설정 추가.
    ex)https://velopert.com/3401

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
 import sample from './modules/sample'//import 리듀서
+
+const middleware = [thunk];
 
 const reducer = combineReducers({sample});
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middleware));
 
 export default store();
